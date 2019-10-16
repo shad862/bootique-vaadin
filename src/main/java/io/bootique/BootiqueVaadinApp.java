e59ad9fcd5c486626b27f200365935965c2ba977
shad862 <shad862@gmail.com>
2019-10-16 09:53:41 +0300

    Vaadin stuff added using WebAppContext and ContextHandlerExtender

--- src/main/java/io/bootique/BootiqueVaadinApp.java
+++ src/main/java/io/bootique/BootiqueVaadinApp.java
@@ -2,35 +2,66 @@ package io.bootique;
 
 import com.google.inject.Binder;
 import com.google.inject.Module;
+import com.vaadin.flow.server.startup.ServletContextListeners;
 import io.bootique.jetty.JettyModule;
-import io.bootique.jetty.server.ServletContextHandlerExtender;
+import io.bootique.utils.Grapher;
 import org.eclipse.jetty.annotations.AnnotationConfiguration;
-import org.eclipse.jetty.servlet.ServletContextHandler;
-import org.eclipse.jetty.webapp.Configuration;
-import org.eclipse.jetty.webapp.WebAppContext;
-import org.eclipse.jetty.webapp.WebInfConfiguration;
+import org.eclipse.jetty.util.resource.Resource;
+import org.eclipse.jetty.webapp.*;
+
+import java.io.IOException;
+import java.net.URI;
+import java.net.URL;
 
 public class BootiqueVaadinApp implements Module {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
         BQRuntime runtime = Bootique.app("-server", "-config=classpath:config.yml")
             .modules(
                 JettyModule.class,
                 BootiqueVaadinApp.class
             )
             .createRuntime();
+        Grapher.printGraph(runtime);
         runtime.run();
+    }
+
+    private WebAppContext buildVaadinWebAppContext() throws Exception {
+        //
+        // https://vaadin.com/tutorials/embedded-jetty-server-in-vaadin-flow
+        //
+
+        URL webRootLocation = JustJettyApplication.class.getResource("/webapp/");
+        URI webRootUri = webRootLocation.toURI();
+
+        WebAppContext context = new WebAppContext();
+        context.setBaseResource(Resource.newResource(webRootUri));
+        context.setContextPath("/");
+        context.setAttribute("org.eclipse.jetty.server.webapp.ContainerIncludeJarPattern", ".*");
+        context.setConfigurationDiscovered(true);
+        context.setConfigurations(new Configuration[]{
+                new AnnotationConfiguration(),
+                new WebInfConfiguration(),
+                new WebXmlConfiguration(),
+                new MetaInfConfiguration()
+        });
+        context.getServletContext().setExtendedListenerTypes(true);
+        context.addEventListener(new ServletContextListeners());
+
+        return context;
     }
 
     @Override
     public void configure(Binder binder) {
-        WebAppContext webAppContext = new WebAppContext();
-        //http://qaru.site/questions/164726/spring-31-webapplicationinitializer-embedded-jetty-8-annotationconfiguration
-        webAppContext.setResourceBase("src/main/webapp");
-        webAppContext.setContextPath("/");
-        webAppContext.setConfigurations(new Configuration[] { new AnnotationConfiguration(), new WebInfConfiguration() });
-        webAppContext.setParentLoaderPriority(true);
-        ServletContextHandler servletContextHandler = new ServletContextHandler();
-
-
+        try {
+            JettyModule.extend(binder).addContextHandlerExtender(extender -> {
+                try {
+                    extender.setHandler(buildVaadinWebAppContext());
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
+            });
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
     }
 }
